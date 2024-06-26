ee1a14e1afea8cf7a2e912c5a929b020a3240272
bappo <boris.atanassov1@gmail.com>
2024-06-26 17:25:51 +0300

    added shit

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
 import pygame
 import random
 import game
-import button
 import entity
+import player
 
-def staticloadEntities():
-    global PlayBtn, ControlsBtn, QuitBtn, backgrounds
-
-    backgrounds = [] 
-    
-    for i in range(2):
-        backgrounds.append(entity.Entity(game.resolution, "Assets/background/Background.png",(i * game.resolution.x, 0), True))
-        
-def draw():
-    game.drawMultiple(backgrounds, (0, 0))
-        
-def scrollBackgrounds(backgrounds, player, game):
-    if player.outOfBounds(game):
-        if backgrounds[1].rect.x <= 0:
-            backgrounds[0].rect.x = backgrounds[1].rect.x + backgrounds[1].rect.width
-        if backgrounds[0].rect.x <= 0:
-            backgrounds[1].rect.x = backgrounds[0].rect.x + backgrounds[0].rect.width    
-        
-
-        for i in range(0, 2):
-            backgrounds[i].rect.x -= backgrounds[i].scrollSpeed
 # Before Starting the Game
 game = game.Game()
-staticloadEntities()
 
+def loadEntities():
+    global player
+    player = player.Player((75, 132), "", (25, game.resolution.y - 131), False)  # place (0, 0) in the position variable
+    player.texture = player.getPlayerSprites()
+
+loadEntities()
 while True:
     while game.startMenu:       
         game.startMenu()
     
     game.getEvent()
-
-    draw()
-    # scrollBackgrounds(backgrounds,)
-
+    game.draw(game.background)
+    player.update()
+    player.invert()
+    print(player.invertSprite)
+    game.draw(player, (player.rect.x, player.rect.y))
     game.update()
     game.clock.tick(60)
- 
