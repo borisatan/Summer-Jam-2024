18c5363e8a15d207e009737fa160099550ace9f7
bappo <boris.atanassov1@gmail.com>
2024-06-26 18:33:16 +0300

    n

--- player.py
+++ player.py
@@ -71,7 +71,7 @@ class Player(entity.Entity):
         return spriteList    
      
     def getPlayerSprites(self):
-        self.idleSprites = self.getPlayerSpriteSubFunction("Assets\Blue\idle.png", 3)
+        self.idleSprites = self.getPlayerSpriteSubFunction("Assets\Blue\idle.png", 4)
         # self.jumpSprites = self.getPlayerSpriteSubFunction("Jump", 2)
         # self.interactSprites = self.getPlayerSpriteSubFunction("Interact", 2)
         # self.runSprites = self.getPlayerSpriteSubFunction("Run", 8)
